81bbc5ed85535dfe3e76e49a6d0c3fdc1c49e692
sneider mendoza <mariasol0304@gmail.com>
2022-05-11 22:42:41 -0500

    creacion de base de datos Mysql,y sus modelos

--- SISTEMA/LIBRERIA/settings.py
+++ SISTEMA/LIBRERIA/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'LIBRERIA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'libreria',
+        'USER': 'root',
+        'PASSWORD': 'Cc1045698090',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- SISTEMA/LibreriAPP/models.py
+++ SISTEMA/LibreriAPP/models.py
@@ -1,3 +1,11 @@
+from distutils.command import upload
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='título')
+    imagen = models.ImageField(upload_to='imagenes/',verbose_name='Imagen', null=True)
+    descripcion = models.TextField(verbose_name='Descripcíon', null=True)
+
+    
